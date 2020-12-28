afe9595ab2ada994386394979a3c838bc9462e31
sunildipun <sunil.6318ie@gmail.com>
2020-12-28 16:11:40 +0530

    added reviews component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 
+import Review from './components/review/review.component';
+
 function App() {
   return (
-    <div className="App">
-      Lets starts reviews 🚀 
+    <section className="container">
+    <div>
+      <div className="title">
+        <h2>Our Reviews</h2>
+        <div className="underline"></div>
+      </div>
+      <Review />
     </div>
+    </section>
   );
 }
 
---
+++ src/components/review/review.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Review = () => {
+    return (
+        <div className="cards">
+            Here is the Reviews
+        </div>
+    )
+}
+
+export default Review;
